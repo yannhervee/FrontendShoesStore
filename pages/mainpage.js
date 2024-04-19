d2f3facdb648c2182ec0e7d6368d30bd68312cbf
praveenvadde <praveenvadde09@gmail.com>
2024-04-19 10:16:43 -0400

    mainpage update

--- pages/mainpage.js
+++ pages/mainpage.js
@@ -9,10 +9,6 @@ export default function Main() {
         <title>Eco-Friendly Shoes Store</title>
         <meta name="description" content="Shop eco-friendly shoes for women with a conscience." />
       </Head>
-      <Link href="/productListing" passHref>
-        <Button component="a">Shop Now</Button>
-    </Link>
-
       {/* Header */}
       <header className="bg-gray-100 py-4 text-center">
         {/* Add your navigation bar here */}
@@ -38,30 +34,43 @@ export default function Main() {
         </section>
 
         {/* Call to action section */}
+        <div className='flex flex-col flex-grow'>
+          <div className='h-screen flex items-center justify-center p-6 bg-gray-200 rounded-lg'>
         <section className="flex items-center justify-center p-6 bg-gray-200 rounded-lg mx-4"
         style={{
           backgroundImage: 'url("C:\Users\admin\OneDrive\Pictures\Screenshots\Rectangle 4.png")',
           backgroundPosition: 'center',
           backgroundSize: 'cover',
           backgroundRepeat: 'no-repeat',
+        
         }}
         >
+          
           <div className="text-center w-full"> {/* Adjust width as necessary */}
           <h2 className="text-2xl font-bold text-gray-800 mb-4">Fashion with a conscience</h2>
           <p className="text-gray-600 mb-6">Eco-Friendly shoes for Women</p>
+          <a href="/productListing">
+
         <button className="bg-green-800 text-white font-bold py-2 px-6 rounded hover:bg-green-600 focus:outline-none focus:shadow-outline">
           Shop Now
         </button>
+        </a>
         </div>
+      
+      
       </section>
-    </main>     
-          {/* Footer */}
+      </div>
+      <div >
+         {/* Footer */}
       <footer className="bg-green-800 self-bottom text-center ">
         <div className="w-1/2 mx-auto text-center">
-         <p className="text-white font-bold mb-0">Not a Member yet?</p> 
+        <p className="text-white font-bold mb-0">Not a Member yet?</p> 
         <p className="text-white font-bold"> Join our Sustainable Fashion Movement!</p>
         </div>
       </footer>
+      </div>
+      </div>
+    </main>     
       
     </div>
 
